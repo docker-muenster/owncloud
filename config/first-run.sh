eeeda5c4fec4daa119991c10e5d2565a02bd3ac0
Tobias Bradtke <webwurst@gmail.com>
2016-03-20 00:29:43 +0100

    Wait for mysql being ready

--- config/first-run.sh
+++ config/first-run.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-set -e
 
 if [ -z "$ADMIN_PASSWORD" ]; then
   exit 1
@@ -9,19 +8,23 @@ ADMIN_USERNAME=${ADMIN_USERNAME:-admin}
 if [ ! -e '/var/www/html/version.php' ]; then
 	tar cf - --one-file-system -C /usr/src/owncloud . | tar xf -
 
-  # sleep 15
-  until netcat -z -w 2 mysql 3306; do sleep 1; done
+  # docker-php-ext-install pcntl
 
-  php occ maintenance:install \
-    --database "mysql" \
-    --database-name "owncloud" \
-    --database-host "mysql" \
-    --database-user "root" \
-    --database-pass "password" \
-    --admin-user "$ADMIN_USERNAME" \
-    --admin-pass "$ADMIN_PASSWORD" \
-    -vv \
-    --no-interaction
+  while :; do
+    php occ maintenance:install \
+      --database "mysql" \
+      --database-name "owncloud" \
+      --database-host "mysql" \
+      --database-user "root" \
+      --database-pass "password" \
+      --admin-user "$ADMIN_USERNAME" \
+      --admin-pass "$ADMIN_PASSWORD" \
+      -vv \
+      --no-interaction
+
+    if [[ $? == 0 ]]; then break; fi
+    sleep 1
+  done
 
 	chown -R www-data /var/www/html
 fi
